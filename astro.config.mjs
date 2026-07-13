ca7aa67953d01e6d593733bec199c6373c8ace34
gu4rdian059-star <gu4rdian059@gmail.com>
2026-07-13 21:29:07 +0700

    Fix Vercel import path

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig, fontProviders } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
